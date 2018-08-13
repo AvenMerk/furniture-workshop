40685bc87dfd0bf7f41e07b560ae341a776ad6bf
AvenMerk <kremnevaaj@gmail.com>
2018-08-13 16:59:51 +0300

    cleaned garbage

--- src/containers/VisibleItemList.js
+++ src/containers/VisibleItemList.js
@@ -1,16 +1,10 @@
 import { connect } from 'react-redux'
-import { toggleTodo } from '../actions'
 import ItemList from '../components/ItemList'
 
 const mapStateToProps = state => ({
   items: state.items
 })
 
-const mapDispatchToProps = dispatch => ({
-  toggleTodo: id => dispatch(toggleTodo(id))
-})
-
 export default connect(
-  mapStateToProps,
-  mapDispatchToProps
+  mapStateToProps
 )(ItemList)
